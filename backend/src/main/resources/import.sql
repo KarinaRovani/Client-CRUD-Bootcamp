4a89dd1fbafe5c0f08a25a271c0c44d54eacc99b
karinarovani <kkarinarovani@gmail.com>
2023-04-20 06:09:33 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into tb_client(name) VALUES ('Ana');
+insert into tb_client(name) VALUES ('José');
+insert into tb_client(name) VALUES ('João');
+insert into tb_client(name) VALUES ('Maria');
