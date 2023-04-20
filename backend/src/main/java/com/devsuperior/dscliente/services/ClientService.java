bde88fa99f77057fa2e7b73c3d03db06acb92ae2
karinarovani <kkarinarovani@gmail.com>
2023-04-20 06:30:03 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dscliente.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscliente.dto.ClientDTO;
 import com.devsuperior.dscliente.entities.Client;
 import com.devsuperior.dscliente.repositories.ClientRepository;
 
@@ -14,8 +17,11 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
-	public List<Client> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 	
 }
