e73dd0f3b7c0ecdb21bde1059234fa33a82c7605
Karina de Oliveira Rovani <kkarinarovani@gmail.com>
2023-05-17 09:09:53 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
@@ -1,14 +1,14 @@
 package com.devsuperior.dscliente.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,10 +25,10 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
-		List<Client> list = repository.findAll();
+	public Page<ClientDTO> findAllPaged(PageRequest PageRequest){
+		Page<Client> list = repository.findAll(PageRequest);
 		
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
